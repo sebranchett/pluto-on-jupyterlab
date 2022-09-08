494519f8b8881aa65027ed78c6a281967e101a64
Susan Branchett <s.e.branchett@tudelft.nl>
2022-09-08 12:06:58 +0200

    extend time-out to give Fargate more time

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -5,7 +5,7 @@ def setup_plutoserver():
   iconpath = os.path.join(mydir, 'icons', 'pluto-logo.svg')
   return {
     "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
-    "timeout": 60,
+    "timeout": 120,
     "launcher_entry": {
         "title": "Pluto.jl",
         "icon_path": iconpath
