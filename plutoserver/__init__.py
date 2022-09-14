a75c2851b393055708ba9b895686f93b8d814a07
sebranchett <s.e.branchett@tudelft.nl>
2022-09-14 10:21:57 +0200

    update and tidy

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -7,8 +7,5 @@ def setup_plutoserver():
     "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
     "timeout": 120,
     "new_browser_tab": True,
-    "launcher_entry": {
-        "title": "Pluto.jl",
-        "icon_path": iconpath
-    },
+    "launcher_entry": {"title": "Pluto.jl", "icon_path": iconpath},
   }
